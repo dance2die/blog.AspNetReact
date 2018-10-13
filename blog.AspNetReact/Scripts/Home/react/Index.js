f9055fb5dc798aa626cb86a2a370879197540597
Sung Kim <sungmkim80@gmail.com>
2018-10-13 14:29:16 -0400

    works well

---
+++ blog.AspNetReact/Scripts/Home/react/Index.js
@@ -0,0 +1,12 @@
+﻿import React, {Fragment} from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <Fragment>
+        <h1>React works in ASP.NET MVC! Yes!</h1>
+        <div>Hello React World!!xxx!</div>
+    </Fragment>
+);
+
+
+render(<App />, document.getElementById('app'));
---
+++ blog.AspNetReact/webpack.config.js
@@ -0,0 +1,34 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+// https://sochix.ru/how-to-integrate-webpack-into-visual-studio-2015/
+module.exports = {
+    entry: "./Scripts/Home/react/index.js",
+    output: {
+        path: path.resolve(__dirname, "./Scripts/dist/Home/react"),
+        filename: "bundle.js"
+    },
+    //devServer: {
+    //    contentBase: ".",
+    //    host: "localhost",
+    //    port: 9000
+    //},
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    // Enable Source Map
+    // https://webpack.js.org/configuration/devtool/
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
